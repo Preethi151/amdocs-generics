4f2e52fa095e7c7b5aa9285bbac205a93fc24f09
amdjsfk39 <amdjsfk39@Amdjfs81.iiht.tech>
2022-08-06 19:06:58 +0530

    initial commit

---
+++ src/main/java/com/train/MyClass.java
@@ -0,0 +1,12 @@
+package com.train;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
